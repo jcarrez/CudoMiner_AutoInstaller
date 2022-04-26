9a93f55dfc47139f10cf9ba0ccba9bfa47498c7a
Jérémie CARREZ <jeremie.carrez@yahoo.fr>
2022-04-26 14:51:43 +0200

    20220426 - 1st Commit

---
+++ install.bat
@@ -0,0 +1,39 @@
+start CudoMiner.exe
+
+echo cruel-tarsier-2|clip
+
+ping localhost -n 120 >nul
+nircmd sendkey enter press
+ping localhost -n 5 >nul
+nircmd sendkey tab press
+ping localhost -n 5 >nul
+nircmd sendkey tab press
+ping localhost -n 5 >nul
+nircmd sendkey tab press
+ping localhost -n 5 >nul
+nircmd sendkey spc press
+ping localhost -n 5 >nul
+nircmd sendkey enter press
+ping localhost -n 5 >nul
+nircmd sendkey c press
+nircmd sendkey r press
+nircmd sendkey u press
+nircmd sendkey e press
+nircmd sendkey l press
+nircmd sendkey subtract press
+nircmd sendkey t press
+nircmd sendkey a press
+nircmd sendkey r press
+nircmd sendkey s press
+nircmd sendkey i press
+nircmd sendkey e press
+nircmd sendkey r press
+nircmd sendkey subtract press
+nircmd sendkey 98 press
+
+ping localhost -n 5 >nul
+nircmd sendkey enter press
+ping localhost -n 5 >nul
+nircmd sendkey enter press
+
+ping localhost -n 240 >nul
